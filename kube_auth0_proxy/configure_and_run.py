324ccb75f78924cebfbe551cb86789670adf492e
Trond Hindenes <trond.hindenes@rikstv.no>
2017-12-28 15:56:49 +0100

    Add port 80 vhost

--- kube_auth0_proxy/configure_and_run.py
+++ kube_auth0_proxy/configure_and_run.py
@@ -64,6 +64,55 @@ config = '''
             ProxyPassReverse <<proxyto>>
         </Location>
 </VirtualHost>
+<VirtualHost *:80>
+        #OIDCProviderIssuer https://<<auth0domain>>
+        OIDCProviderMetadataURL https://<<auth0domain>>/.well-known/openid-configuration
+        #OIDCProviderAuthorizationEndpoint https://<<auth0domain>>/authorize
+        #OIDCProviderTokenEndpoint https://<<auth0domain>>/oauth/token
+        #OIDCProviderTokenEndpointAuth client_secret_post
+        OIDCProviderUserInfoEndpoint https://<<auth0domain>>/userinfo
+        OIDCSessionMaxDuration 0
+        OIDCSessionInactivityTimeout <<session_timeout>>
+
+        OIDCClientID <<auth0clientid>>
+        OIDCClientSecret <<auth0clientsecret>>
+
+        OIDCScope "openid profile email phone address"
+        #This should point to the public endpoint of your apache proxy
+        OIDCRedirectURI <<thiscontainerurl>>auth/
+        OIDCCryptoPassphrase <password>
+        OIDCCookiePath /
+        OIDCScrubRequestHeaders Off
+        OIDCPassIDTokenAs serialized
+        OIDCPassClaimsAs both
+        OIDCIDTokenSignedResponseAlg RS256
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/html
+
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+        ErrorLog /dev/stderr
+
+        #Some sites have large payloads
+        LimitRequestFieldSize 32768
+
+        RequestHeader set Authorization "Bearer %{OIDC_id_token}e"
+
+        <Location /auth/>
+            AuthType openid-connect
+            Require valid-user
+            LogLevel debug
+        </Location>
+
+        <Location />
+            AuthType openid-connect
+            Require valid-user
+            
+            LogLevel debug
+            ProxyPass <<proxyto>>
+            ProxyPassReverse <<proxyto>>
+        </Location>
+</VirtualHost>
 # vim: syntax=apache ts=4 sw=4 sts=4 sr noet
 '''
 
